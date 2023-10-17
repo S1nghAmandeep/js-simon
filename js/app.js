28a4a3c0a51cf146f1cd4541aebb29c6431252b3
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-10-17 15:26:55 +0200

    added-random-5-numbers-generator

---
+++ js/app.js
@@ -0,0 +1,29 @@
+
+const randomNumber = getNumberForArray(1, 100, 5);
+console.log(randomNumber)
+alert('ricorda questi numeri ' + randomNumber + '. Dopo aver cliccato su ok aspetta 3 secondi')
+
+function getNumberForArray (randomMin, randomMax, totalNumber) {
+
+    const arrayNumbers = [];
+
+    while (arrayNumbers.length < totalNumber) {
+        const number = getRandomInt(randomMin, randomMax);
+
+        if (!arrayNumbers.includes(number)){
+            arrayNumbers.push(number);
+        }
+    }
+    return arrayNumbers
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+
+
+
